69add09f333a94bed8ea9e38c5525143f158ed1b
emoncse2020 <emon.cse.2020@gmail.com>
2025-08-10 14:16:55 +0600

    project done

--- trip/views.py
+++ trip/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from django.views.generic import TemplateView, CreateView, DetailView
+from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView
 
+
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 from .models import Trip, Note
@@ -11,7 +13,7 @@ from .models import Trip, Note
 class HomeView(TemplateView):
     template_name = 'trip/index.html'
 
-
+@login_required(login_url='login')
 def trips_list(request):
     trips = Trip.objects.filter(owner=request.user)
     context = {
@@ -45,3 +47,49 @@ class TripDetailView(DetailView):
 
 class NoteDetailView(DetailView):
     model = Note
+
+class NoteListView(ListView):
+    model = Note
+
+    def get_queryset(self):
+        queryset = Note.objects.filter(trip__owner = self.request.user)
+        return queryset
+    
+class NoteCreateView(CreateView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    fields = "__all__"
+
+    def get_form(self):
+        form = super(NoteCreateView, self).get_form()
+        trips = Trip.objects.filter(owner = self.request.user)
+        form.fields['trip'].queryset = trips
+        return form
+    
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+    fields = "__all__"
+
+    def get_form(self):
+        form = super(NoteUpdateView, self).get_form()
+        trips = Trip.objects.filter(owner = self.request.user)
+        form.fields['trip'].queryset = trips
+        return form
+    
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    success_url = reverse_lazy('note-list')
+
+
+class TripUpdateView(UpdateView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+    fields = ['city', 'country', 'start_date', 'end_date']
+
+class TripDeleteView(DeleteView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+
